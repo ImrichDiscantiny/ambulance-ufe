2fd147e18d19e2eb84cdb6dd76a141a352b2ac07
ImrichDiscantiny <55829428+ImrichDiscantiny@users.noreply.github.com>
2024-03-05 12:39:07 +0100

    fixes

--- src/components/id-ambulance-wl-app/test/id-ambulance-wl-app.spec.tsx
+++ src/components/id-ambulance-wl-app/test/id-ambulance-wl-app.spec.tsx
@@ -2,17 +2,27 @@ import { newSpecPage } from '@stencil/core/testing';
 import { IdAmbulanceWlApp } from '../id-ambulance-wl-app';
 
 describe('id-ambulance-wl-app', () => {
-  it('renders', async () => {
+
+  it('renders editor', async () => {
     const page = await newSpecPage({
+      url: `http://localhost/entry/@new`,
       components: [IdAmbulanceWlApp],
-      html: `<id-ambulance-wl-app></id-ambulance-wl-app>`,
+      html: `<id-ambulance-wl-app base-path="/"></id-ambulance-wl-app>`,
     });
-    expect(page.root).toEqualHtml(`
-      <id-ambulance-wl-app>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </id-ambulance-wl-app>
-    `);
+    page.win.navigation = new EventTarget()
+    const child = await page.root.shadowRoot.firstElementChild;
+    expect(child.tagName.toLocaleLowerCase()).toEqual ("<id-ambulance-wl-editor");
+
+  });
+
+  it('renders list', async () => {
+    const page = await newSpecPage({
+      url: `http://localhost/ambulance-wl/`,
+      components: [IdAmbulanceWlApp],
+      html: `<id-ambulance-wl-app base-path="/ambulance-wl/"></id-ambulance-wl-app>`,
+    });
+    page.win.navigation = new EventTarget()
+    const child = await page.root.shadowRoot.firstElementChild;
+    expect(child.tagName.toLocaleLowerCase()).toEqual("id-ambdulance-wl-list");
   });
 });
